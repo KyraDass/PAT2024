132d372cddba32a446d0a8b6bcc7571cac3125b6
kyrad <kyrad@10.10.6.109>
2024-10-14 21:36:11 +0200

    now there is an error

---
+++ src/main/java/UI/LoginAndSignUp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package UI;
+
+/**
+ *
+ * @author kyrad
+ */
+public class LoginAndSignUp {
+
+    public static void main(String[] args) {
+        
+         Login LoginFrame = new Login();
+            LoginFrame.setVisible(true);
+            LoginFrame.pack();
+            LoginFrame.setLocationRelativeTo(null); // centre
+         
+       
+    }
+}
