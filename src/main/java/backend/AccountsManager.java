f9076052ab2d19f4c4b9e787fabe5f584256c7e8
kyrad <kyrad@10.10.6.109>
2024-10-14 21:37:19 +0200

    now there is an error

---
+++ src/main/java/backend/AccountsManager.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kyrad
+ */
+public class AccountsManager {
+    
+    public static String fileName = "Data\\AccountList.txt";
+
+    public static String[] getAccountNames() throws FileNotFoundException {
+
+        File accountsFile = new File(fileName);
+        Scanner accountSc = new Scanner(accountsFile);
+
+        int count = 0;
+        while (accountSc.hasNext()) {
+            count++;
+            accountSc.next();
+            System.out.println("");
+
+        }
+
+        accountSc = new Scanner(accountsFile);
+        String[] account = new String[count];
+        for (int i = 0; i < count; i++) {
+            account[i] = accountSc.next();
+
+        }
+        return account;
+    }
+
+    public static void deleteAccount(String account) throws FileNotFoundException, IOException {
+        File accountsFile = new File(fileName);
+        Scanner accountSc = new Scanner(accountsFile);
+
+        String line = "";
+        while (accountSc.hasNext()) {
+            String currentAccount = accountSc.next();
+            if (!currentAccount.equals(account)) {
+                line = line + currentAccount + " ";
+
+            }
+
+            FileWriter accountFileWriter = new FileWriter(fileName);
+            PrintWriter pw = new PrintWriter(accountFileWriter);
+            pw.write(line);
+            pw.close();
+
+        }
+
+    }
+
+    public static void addAccount(String account) throws FileNotFoundException, IOException {
+        File accountsFile = new File(fileName);
+        Scanner accountSc = new Scanner(accountsFile);
+        String line = accountSc.nextLine() + " " + account;
+
+        FileWriter accountFileWriter = new FileWriter(fileName);
+        PrintWriter pw = new PrintWriter(accountFileWriter);
+        pw.write(line);
+        pw.close();
+    }
+    
+    
+
+}
